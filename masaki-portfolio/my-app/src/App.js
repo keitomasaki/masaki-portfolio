a351819bb621e9b468e4ba297e4ffd58df5fceb8
masaki <keitomasaki@icloud.com>
2021-06-15 00:35:37 +0900

    react-router-domを導入

--- masaki-portfolio/my-app/src/App.js
+++ masaki-portfolio/my-app/src/App.js
@@ -1,12 +1,57 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <p>正木のサイトです</p>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/page_a">Page A</Link>
+        </li>
+        <li>
+          <Link to="/page_b">Page B</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/page_a" component={PageA} />
+      <Route path="/page_b" component={PageB} />
+    </Switch>
+  </main>
+);
+
+const Home = () => (
+  <div>
+    <h1> Home!!!!! </h1>
+  </div>
+);
+
+const PageA = () => (
+  <div>
+    <h1> Page A!!!!! </h1>
+  </div>
+);
+
+const PageB = () => (
+  <div>
+    <h1> Page B!!!!! </h1>
+  </div>
+);
 
 export default App;
